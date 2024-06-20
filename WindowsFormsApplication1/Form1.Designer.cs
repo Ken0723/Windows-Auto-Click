f631ecdd0b890b214a7c351c851e9b2f57c8c060
Ken0723 <43611876+Ken0723@users.noreply.github.com>
2024-06-20 10:38:46 +0800

    Init

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lsbAsyncTaskVal = new System.Windows.Forms.ListBox();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.btnGetXY = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblXVal = new System.Windows.Forms.Label();
+            this.lblYVal = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lstApplication = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(9, 293);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 38);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(482, 293);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(112, 38);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lsbAsyncTaskVal
+            // 
+            this.lsbAsyncTaskVal.FormattingEnabled = true;
+            this.lsbAsyncTaskVal.ItemHeight = 20;
+            this.lsbAsyncTaskVal.Location = new System.Drawing.Point(9, 10);
+            this.lsbAsyncTaskVal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lsbAsyncTaskVal.Name = "lsbAsyncTaskVal";
+            this.lsbAsyncTaskVal.Size = new System.Drawing.Size(583, 184);
+            this.lsbAsyncTaskVal.TabIndex = 3;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(9, 17);
+            this.lblX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(28, 20);
+            this.lblX.TabIndex = 4;
+            this.lblX.Text = "X: ";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(9, 48);
+            this.lblY.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(28, 20);
+            this.lblY.TabIndex = 5;
+            this.lblY.Text = "Y: ";
+            // 
+            // btnGetXY
+            // 
+            this.btnGetXY.Location = new System.Drawing.Point(482, 245);
+            this.btnGetXY.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGetXY.Name = "btnGetXY";
+            this.btnGetXY.Size = new System.Drawing.Size(112, 38);
+            this.btnGetXY.TabIndex = 6;
+            this.btnGetXY.Text = "Get X, Y";
+            this.btnGetXY.UseVisualStyleBackColor = true;
+            this.btnGetXY.Click += new System.EventHandler(this.btnGetXY_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 7F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(432, 222);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "*Press F4 to get x, y";
+            // 
+            // lblXVal
+            // 
+            this.lblXVal.AutoSize = true;
+            this.lblXVal.Location = new System.Drawing.Point(46, 15);
+            this.lblXVal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblXVal.Name = "lblXVal";
+            this.lblXVal.Size = new System.Drawing.Size(0, 20);
+            this.lblXVal.TabIndex = 8;
+            // 
+            // lblYVal
+            // 
+            this.lblYVal.AutoSize = true;
+            this.lblYVal.Location = new System.Drawing.Point(46, 47);
+            this.lblYVal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblYVal.Name = "lblYVal";
+            this.lblYVal.Size = new System.Drawing.Size(0, 20);
+            this.lblYVal.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblYVal);
+            this.groupBox1.Controls.Add(this.lblXVal);
+            this.groupBox1.Controls.Add(this.lblX);
+            this.groupBox1.Controls.Add(this.lblY);
+            this.groupBox1.Location = new System.Drawing.Point(9, 207);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(112, 77);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(18, 20);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(615, 395);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lsbAsyncTaskVal);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnStop);
+            this.tabPage1.Controls.Add(this.btnGetXY);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Size = new System.Drawing.Size(607, 362);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Task Setting";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnRefresh);
+            this.tabPage2.Controls.Add(this.lstApplication);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Size = new System.Drawing.Size(607, 362);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Application List";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(482, 302);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(112, 38);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lstApplication
+            // 
+            this.lstApplication.FormattingEnabled = true;
+            this.lstApplication.ItemHeight = 20;
+            this.lstApplication.Location = new System.Drawing.Point(4, 5);
+            this.lstApplication.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstApplication.Name = "lstApplication";
+            this.lstApplication.Size = new System.Drawing.Size(592, 284);
+            this.lstApplication.TabIndex = 0;
+            this.lstApplication.SelectedIndexChanged += new System.EventHandler(this.lstApplication_SelectedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 435);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "My Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lsbAsyncTaskVal;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Button btnGetXY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblXVal;
+        private System.Windows.Forms.Label lblYVal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox lstApplication;
+        private System.Windows.Forms.Button btnRefresh;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("User32")]
+        public static extern void mouse_event(
+            int dwFlags,
+            int dx,
+            int dy,
+            int dwData,
+            int dwExtraInfo
+        );
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern bool BlockInput(bool block);
+
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("User32.dll")]
+        public static extern int FindWindowEx(int hwndParent, int hwndChildAfter, string strClassName, string strWindowName);
+
+        Boolean isReadRunning = false;
+        Boolean isautoClick1Running = false;
+        Boolean isGettingXY = false;
+        Thread autoThread1, ky, gettingXY;
+
+        private System.Int32 iHandle;
+        private Process[] currentProc;
+
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        const int MOUSEEVENTF_WHEEL = 0x0800;
+        const int MOUSEEVENTF_XDOWN = 0x0080;
+        const int MOUSEEVENTF_XUP = 0x1000;
+        const int MOUSEEVENTF_HWHEEL = 0x01000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            btnStop.Enabled = false;
+            btnStart.Enabled = false;
+            getRunningApplication();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            readKeyboard();
+            lsbAsyncTaskVal.Items.Add("Starting...");
+        }
+
+        private void readKeyboard()
+        {
+            isReadRunning = true;
+            //lsbAsyncTaskVal.Items.Clear();
+            if (ky == null)
+            {
+                ky = new Thread(Keyboardd);
+                ky.SetApartmentState(ApartmentState.STA);
+                CheckForIllegalCrossThreadCalls = false;
+                isReadRunning = true;
+                ky.Start();
+                lsbAsyncTaskVal.Items.Add("Completed");
+            }
+        }
+
+        private void getRunningApplication()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    lstApplication.Items.Add(p.MainWindowTitle);
+                }
+            }
+        }   
+
+        void Keyboardd()
+        {
+            while (isReadRunning)
+            {
+                Thread.Sleep(40);
+
+                if ((Keyboard.GetKeyStates(Key.F2) & KeyStates.Down) > 0)
+                {
+                    if (autoThread1 == null)
+                    {
+                        lsbAsyncTaskVal.Items.Add("Auto Clicking...");
+                        autoThread1 = new Thread(autoClick1);
+                        autoThread1.SetApartmentState(ApartmentState.STA);
+                        CheckForIllegalCrossThreadCalls = false;
+                        isautoClick1Running = true;
+                        autoThread1.Start();
+
+                        btnStop.Enabled = true;
+                        continue;
+                    }
+
+                    if (autoThread1.IsAlive)
+                    {
+                        MousePoint currentMousePoint;
+                        var gotPoint = GetCursorPos(out currentMousePoint);
+
+                        lsbAsyncTaskVal.Items.Add("Auto Clicking....");
+                        autoThread1 = new Thread(autoClick1);
+                        autoThread1.SetApartmentState(ApartmentState.STA);
+                        CheckForIllegalCrossThreadCalls = false;
+                        isautoClick1Running = true;
+                        autoThread1.Start();
+
+                        btnStop.Enabled = true;
+                        continue;
+                    }
+                }
+
+                if ((Keyboard.GetKeyStates(Key.F3) & KeyStates.Down) > 0)
+                {
+                    if (autoThread1 == null)
+                    {
+                        continue;
+                    }
+                    if (autoThread1.IsAlive)
+                    {
+                        ky = null;
+                        autoThread1 = null;
+                        lsbAsyncTaskVal.Items.Add("Stop!");
+                        isautoClick1Running = false;
+                        btnStop.Enabled = false;
+                        BlockInput(false);
+                    }
+                }
+
+            }
+        }
+
+        void autoClick1()
+        {
+            while (isautoClick1Running)
+            {
+                Thread.Sleep(50);
+                SetCursorPos(int.Parse(lblXVal.Text.ToString()), int.Parse(lblYVal.Text.ToString()));
+                //BlockInput(true);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, int.Parse(lblXVal.Text.ToString()), int.Parse(lblYVal.Text.ToString()), 0, 0);
+            }
+        }
+
+        void getXY()
+        {
+            while (isGettingXY)
+            {
+                Thread.Sleep(50);
+                if ((Keyboard.GetKeyStates(Key.F4) & KeyStates.Down) > 0)
+                {
+                    MousePoint currentMousePoint;
+                    var gotPoint = GetCursorPos(out currentMousePoint);
+
+                    int x, y;
+                    x = currentMousePoint.X;
+                    y = currentMousePoint.Y;
+                    lblXVal.Text = x.ToString();
+                    lblYVal.Text = y.ToString();
+
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnStart.Enabled = true;
+                    isGettingXY = false;
+                    gettingXY = null;
+                    btnGetXY.Enabled = true;
+                    System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            ky = null;
+            autoThread1 = null;
+            isReadRunning = false;
+            isautoClick1Running = false;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        private void btnGetXY_Click(object sender, EventArgs e)
+        {
+            if (gettingXY == null)
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+                gettingXY = new Thread(getXY);
+                gettingXY.SetApartmentState(ApartmentState.STA);
+                CheckForIllegalCrossThreadCalls = false;
+                isGettingXY = true;
+                gettingXY.Start();
+                btnGetXY.Enabled = false;
+            }
+        }
+
+        private void lstApplication_SelectedChanged(object sender, EventArgs e)
+        {
+            String appName = lstApplication.SelectedItem.ToString();
+            iHandle = FindWindow("", appName);
+       } 
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lstApplication.Items.Clear();
+            getRunningApplication();
+        }
+    }
+}
